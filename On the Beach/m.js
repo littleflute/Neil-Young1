eb581bb7ee329d5e76bdc6d8b8040fe917673162
littleflute <13699175164@sina.cn>
2017-07-13 15:35:44 -0500

    Add files via upload

---
+++ On the Beach/m.js
@@ -0,0 +1,10 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Walk On.mp3";		
+f[2]	= "02 See the Sky About to Rain.mp3";		
+f[3]	= "03 Revolution Blues.mp3";		
+f[4]	= "04 For the Turnstiles.mp3";		
+f[5]	= "05 Vampire Blues.mp3";		
+f[6]	= "06 On the Beach.mp3";		
+f[7]	= "07 Motion Pictures.mp3";		
+f[8]	= "08 Ambulance Blues.mp3";		
